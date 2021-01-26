8dd3687ae3326ee70e105cc823da3a875deed3da
spettigrew <sara.pettigrew5@gmail.com>
2021-01-26 16:51:12 -0700

    Added a delete handler in Home, passed in props

--- src/Home.js
+++ src/Home.js
@@ -6,10 +6,15 @@ const Home = () => {
         {title: "Welcome", body: "paragraph sentence, etc.", author: "Me, myself and I", id: 1,}
     ]);
 
+    const handleDelete = (id) => { 
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" />
-            <BlogList blogs={blogs.filter((blog) => blog.author === "Sara")} title="Sara's Blogs" />
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            
         </div>
      );
 }
