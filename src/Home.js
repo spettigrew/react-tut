a6dab02fced44da28ff029dd3dc0de0b0d19688f
spettigrew <sara.pettigrew5@gmail.com>
2021-01-25 20:34:22 -0700

    Added state hooks to Home component

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [ name, setName] = useState('her');
+    const [ age, setAge ] = useState(30)
+
+    const handleClick = (e) => {
+        setName('she');
+        setAge(40);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } is old.</p>
+            <button onClick={handleClick}>Click Here</button>
         </div>
      );
 }
