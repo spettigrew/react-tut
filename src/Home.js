418de5809dd6603194a3d524c5bd6b9c96b7c963
spettigrew <sara.pettigrew5@gmail.com>
2021-01-26 17:43:26 -0700

    Set up dependency for name in Home comp

--- src/Home.js
+++ src/Home.js
@@ -6,18 +6,22 @@ const Home = () => {
         {title: "Welcome", body: "paragraph sentence, etc.", author: "Me, myself and I", id: 1,}
     ]);
 
+    const [name, setName] = useState('Sara');
+
     const handleDelete = (id) => { 
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
-    // runs code at every render
+    // runs a function after every render
     useEffect(() => {
         console.log('use effect');
-    });
+    }, [name]);
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            <button onClick={() => setName('she')}>change name</button>
+            <p>{ name }</p>
             
         </div>
      );
