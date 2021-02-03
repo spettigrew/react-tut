713e019f75459d0f28f9fa9efb5da092306518c7
spettigrew <sara.pettigrew5@gmail.com>
2021-02-02 17:53:32 -0700

    Added browser router to app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import Navbar from './NavBar';
-import Home from './Home'
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 // React component that returns function = App
 function App() {
   
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        {/* dynamic value =  */}
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          {/* dynamic value =  */}
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
