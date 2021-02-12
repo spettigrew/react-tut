48a97acacb36e8be11297168f8478d2679117c46
spettigrew <sara.pettigrew5@gmail.com>
2021-02-11 18:59:35 -0700

    Created and set state to blog page comp and design

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,30 @@
 const Create = () => {
+    const [title, setTitle] = useState('');
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>
+                    Blog title:
+                    </label>
+                    <input 
+                    type='text'
+                    required
+                    value={title}
+                    onChange={() => setTitle()}
+                    />
+                    <label>Blog body:</label>
+                    <textarea 
+                    required>
+                    </textarea>
+                    <label>Blog author:</label>
+                    <select>
+                        <option value="name">name</option>
+                        <option value="your name">your name</option>
+                        <button>Add Blog</button>
+                    </select>
+            </form>
         </div>
      );
 }
