6d27f14c7fa24feab6176939a075c972fbb7757c
spettigrew <sara.pettigrew5@gmail.com>
2021-04-27 22:20:22 -0600

    Onclick handling added

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,19 @@
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import useFetch from './useFetch';
 
+
 const  BlogDetails = () => {
+    // route parameters from the router
     const{ id } =  useParams();
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id)
+    const history = useHistory
+
+    const handleClick= () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {method: 'Delete'
+    }).then(() => {
+        history.push('/')
+    })
+    }
     return (
         <div className='blog-details'>
             {/* && = the thing on the right only outputs if the thing on the left is true */}
@@ -14,6 +24,7 @@ const  BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by: { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick={handleClick}>delete</button>
                 </article>
             )}
         </div>
